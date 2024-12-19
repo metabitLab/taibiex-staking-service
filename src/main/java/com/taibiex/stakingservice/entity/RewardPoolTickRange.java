ec4a53ef7bdb2450117b4cd266386eb8bfd16515
andi2233i <anyfirew@outlook.com>
2024-12-19 10:12:15 +0800

    fix JoinColumn bug

--- src/main/java/com/taibiex/stakingservice/entity/RewardPoolTickRange.java
+++ src/main/java/com/taibiex/stakingservice/entity/RewardPoolTickRange.java
@@ -1,23 +1,15 @@
 package com.taibiex.stakingservice.entity;
 
-import com.taibiex.stakingservice.common.chain.contract.listener.filter.events.ContractsEventEnum;
-import com.taibiex.stakingservice.common.chain.contract.listener.filter.events.consumer.MintEventHandler;
-import com.taibiex.stakingservice.common.chain.contract.listener.filter.events.impl.ContractsEventBuilder;
 import com.taibiex.stakingservice.common.hibernate.Comment;
-import com.taibiex.stakingservice.config.ContractsConfig;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.web3j.abi.EventEncoder;
-import org.web3j.abi.datatypes.Event;
-import org.web3j.protocol.core.methods.response.Log;
 
 import java.math.BigInteger;
 
 /**
  * 后台配置的对应池子的奖励区间范围，即哪些tick范围内，给多少比例的奖励
- *
  */
 
 //在 JPA 中，@Index 注解的 columnList 属性填写的是数据库表中的列名称，而不是 Java 类中的属性名称
@@ -30,7 +22,7 @@ import java.math.BigInteger;
 @AllArgsConstructor
 @NoArgsConstructor
 @Comment("后台配置 奖励的池子对应的价格区间（及奖励）的列表")
-public class RewardPoolTickRange extends BaseEntity{
+public class RewardPoolTickRange extends BaseEntity {
 
     @Comment("流动性池地址")
     @Column(name = "pool", nullable = false, length = 100)
@@ -38,9 +30,9 @@ public class RewardPoolTickRange extends BaseEntity{
 
     /**
      * Uniswap V3 的 tick 值通常在 -887272 到 887272 之间。超出该范围可能会导致计算错误。
-     *
+     * <p>
      * function tickToPrice(tick) {
-     *     return Math.pow(1.0001, tick);
+     * return Math.pow(1.0001, tick);
      * }
      */
     @Column(name = "tick_lower", nullable = false, length = 64)
@@ -58,23 +50,25 @@ public class RewardPoolTickRange extends BaseEntity{
 
     /**
      * 在
+     *
      * @JoinColumn(name = "reward_pool_id") 指定外键列的名称，表示在 RewardPoolTickRange 表中存储 RewardPool 的 ID。
-    name 属性的值应该与数据库中对应的外键列的名称完全匹配。如果数据库中外键列的名称是 reward_pool_id，
-    那么在 @JoinColumn 中 name 属性也应该设置为 "reward_pool_id"。
+     * name 属性的值应该与数据库中对应的外键列的名称完全匹配。如果数据库中外键列的名称是 reward_pool_id，
+     * 那么在 @JoinColumn 中 name 属性也应该设置为 "reward_pool_id"。
      */
     @ManyToOne(fetch = FetchType.EAGER) //https://blog.csdn.net/qq_43618881/article/details/105214416
     //JoinColumn 指定外键列：nullable = true 表示在 RewardPoolTickRange 表中有一个 reward_pool_id 列，但不在数据库中强制执行外键约束
-    @JoinColumn(name = "reward_pool_id", nullable = true, foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT)) // 允许为 null
+    @JoinColumn(name = "reward_pool_id", referencedColumnName = "id", nullable = true, foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+    // 允许为 null
     private RewardPool rewardPool;
 
 
     /**
      * 获取指定tick对应的价格，Uniswap V3 的 tick 值通常在 -887272 到 887272 之间。超出该范围可能会导致计算错误。
+     *
      * @param tick
      * @return
      */
-    public Double getPriceFromTick(Integer tick)
-    {
+    public Double getPriceFromTick(Integer tick) {
         return Math.pow(1.0001, tick);
     }
 
